0e562e505c99674977136f0eb1187fddaa56d3ce
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2021-01-02 00:31:45 +0800

    hotfix

---
+++ database/migrations/2021_01_01_161136_alter_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropUnique(['username']);
+            $table->dropUnique(['discriminator']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
